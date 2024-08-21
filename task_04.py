da5965127b6066a8fb8d43a3337b667541b77987
Yevhen Mazur <yevhen.mazur@pm.me>
2024-08-21 15:43:33 +0200

    Cosmetic changes

--- task_04.py
+++ task_04.py
@@ -10,12 +10,14 @@ class Node:
         self.right = None
         self.val = key
         self.color = color  # Додатковий аргумент для зберігання кольору вузла
-        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+        # Унікальний ідентифікатор для кожного вузла
+        self.id = str(uuid.uuid4())
 
 
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+       # Використання id та збереження значення вузла
+        graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
@@ -25,7 +27,8 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+            r = add_edges(graph, node.right, pos, x=r,
+                          y=y - 1, layer=layer + 1)
     return graph
 
 
@@ -35,10 +38,12 @@ def draw_tree(tree_root):
     tree = add_edges(tree, tree_root, pos)
 
     colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(
+        data=True)}  # Використовуйте значення вузла для міток
 
     plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    nx.draw(tree, pos=pos, labels=labels, arrows=False,
+            node_size=2500, node_color=colors)
     plt.show()
 
 
@@ -48,10 +53,10 @@ def build_heap_tree(heap, index=0):
         return None
 
     root = Node(heap[index])
-    
+
     root.left = build_heap_tree(heap, 2 * index + 1)
     root.right = build_heap_tree(heap, 2 * index + 2)
-    
+
     return root
 
 
